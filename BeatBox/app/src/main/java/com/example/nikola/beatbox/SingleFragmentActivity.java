7525ab9479c4615b68a9a8169acb4a49a09eb429
NikolaMr <nikola.mrzljak@fer.hr>
2018-09-17 07:13:25 +0200

    Implemented first chapter of BeatBox app.

---
+++ BeatBox/app/src/main/java/com/example/nikola/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,38 @@
+package com.example.nikola.beatbox;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nikola on 23-Aug-18.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.fragment_activity;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
